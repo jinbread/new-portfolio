1017fa6db1afccc29ae7997eab9fec6c1782940a
Jinjae Lee <jinjaelee@Administrators-MacBook-Pro-681.local>
2019-08-22 17:00:37 +0200

    add anime.js text animation

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: function(el, i) {
+      return 50 * i;
+    }
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
